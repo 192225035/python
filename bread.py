375b3c6afa8ec53dbade1de62e10345e3284c3a1
192225035 <124116698+192225035@users.noreply.github.com>
2023-02-01 20:27:57 +0530

    Add files via upload

---
+++ bread.py
@@ -0,0 +1,10 @@
+a=float(input("enter the number of fresh loaves purchased"))
+b=float(input("enter the number of day old loaves purchased"))
+c=a*185
+d=b*185
+e=float(d*60/100)
+f=185
+print("regular price",f)
+print("the fresh bread purchased",c)
+print("the day old bread purchased",e)
+print("total cost of bread",c+e)
---
+++ simplecalculater.py
@@ -0,0 +1,12 @@
+a=int(input("enter the value of a:"))
+b=int(input("enter the value of B:"))
+
+add=a+b
+sub=a-b
+multi=a*b
+div=a/b
+
+print("the addition value:",add)
+print("the substraction value:",sub)
+print("the multiplication value:",multi)
+print("the division value:",div)
